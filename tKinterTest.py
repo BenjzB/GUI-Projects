c875f3160a9bde2f699f72d077532e977993d545
BenjzB <77694685+BenjzB@users.noreply.github.com>
2021-05-07 11:47:40 -0600

    Add files via upload

--- tKinterTest.py
+++ tKinterTest.py
@@ -9,7 +9,7 @@ def results():
 def addToList():
     newItem = E1.get()
     playList.append(newItem)
-
+    E1.delete(0, END)
 
 #This is a Label widget
 L1 = Label(top, text = "Playlist Maker")
@@ -20,10 +20,10 @@ E1 = Entry(top, bd = 5)
 E1.grid(column = 0, row = 2)
 
 #This is a Button widget
-B1 = Button(text = "    Print Playlist    ", bg = "red", command = results)
+B1 = Button(text = "    Print Playlist    ", bg = "#c6edee", command = results)
 B1.grid(column = 0, row = 3)
 
-B2 = Button(text = "Add Song", bg = "green", command = addToList)
+B2 = Button(text = "+", bg = "#feebe5", command = addToList)
 B2.grid(column = 1, row = 3)
 
 
